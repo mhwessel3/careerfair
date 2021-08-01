6cd7a65d7b9a16befd9bb5b15c04686564999515
Shoshanna Perchanok <sp26@illinois.edu>
2021-08-01 09:59:06 -0500

    converted class components to functions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,23 @@
 import React from "react";
 
-class Footer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="footer">
-        Organized by Engineering Council
-        <br />
-        Icons made by{" "}
-        <a
-          href="https://www.flaticon.com/authors/cole-bemis"
-          title="Cole Bemis"
-        >
-          Cole Bemis
-        </a>{" "}
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </div>
-    );
-  }
+function Footer(props){
+  return (
+          <div className="footer">
+            Organized by Engineering Council
+            <br />
+            Icons made by{" "}
+            <a
+              href="https://www.flaticon.com/authors/cole-bemis"
+              title="Cole Bemis"
+            >
+              Cole Bemis
+            </a>{" "}
+            from{" "}
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              www.flaticon.com
+            </a>
+          </div>
+        );
 }
 
 export default Footer;
--- src/components/InterviewLocations.js
+++ src/components/InterviewLocations.js
@@ -1,33 +1,26 @@
 import React from "react";
 
-class InterviewLocations extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="interviewlocations">
-        <h2 id="content_header">interview locations</h2>
-        <div className="info">
-          Below are interview handbooks based on each specific interview
-          location. Click the link below to download the handbook.
-        </div>
-        <a href="../interview-handbooks/DCL.pdf" download>
-          Digital Computer Labratory
-        </a>
-        <a href="../interview-handbooks/Illini-Union.pdf" download>
-          Illini Union
-        </a>
-        <a href="../interview-handbooks/Memorial-Stadium.pdf" download>
-          Memorial Stadium
-        </a>
-        <a href="../interview-handbooks/The-Career-Center.pdf" download>
-          The Career Center
-        </a>
+function InterviewLocations(props){
+  return (
+    <div className="interviewlocations">
+      <h2 id="content_header">interview locations</h2>
+      <div className="info">
+        Below are interview handbooks based on each specific interview
+        location. Click the link below to download the handbook.
       </div>
-    );
-  }
+      <a href="../interview-handbooks/DCL.pdf" download>
+        Digital Computer Labratory
+      </a>
+      <a href="../interview-handbooks/Illini-Union.pdf" download>
+        Illini Union
+      </a>
+      <a href="../interview-handbooks/Memorial-Stadium.pdf" download>
+        Memorial Stadium
+      </a>
+      <a href="../interview-handbooks/The-Career-Center.pdf" download>
+        The Career Center
+      </a>
+    </div>
+  );
 }
-
 export default InterviewLocations;
